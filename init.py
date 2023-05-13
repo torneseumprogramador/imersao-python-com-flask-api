aac88eb4c085e1b5253ffb2c0711a4591f7f808b
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-05-13 13:36:21 -0300

    front-end

--- init.py
+++ init.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_cors import CORS
 
 import aplicacao.rotas.config as rotas
 
 app = Flask(__name__)
-
 rotas.registrar(app)
+CORS(app)
 
 if __name__ == "__main__":
     app.run()
